2dc428c5e3284d43df6b7dd0cbdb1fcac766e660
Artem Vremere <vremere_a@ukr.net>
2020-09-02 15:06:50 +0300

    create db - Storage

---
+++ src/main/java/shop/db/Storage.java
@@ -0,0 +1,10 @@
+package shop.db;
+
+import shop.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage {
+    public static final List<Product> products = new ArrayList<>();
+}
