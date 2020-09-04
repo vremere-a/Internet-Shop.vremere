5f3fa39de6d99e6dfefbc953817c0de10b7a724f
Artem Vremere <vremere_a@ukr.net>
2020-09-04 08:33:04 +0300

    add in Storage other add methods

--- src/main/java/com/internet/shop/db/Storage.java
+++ src/main/java/com/internet/shop/db/Storage.java
@@ -13,9 +13,27 @@ public class Storage {
     public static final List<ShoppingCart> shoppingCarts = new ArrayList<>();
     public static final List<User> users = new ArrayList<>();
     private static Long productId = 0L;
+    private static Long userId = 0L;
+    private static Long orderId =0L;
+    private static Long cartId = 0L;
 
     public static void addProduct(Product product) {
         product.setId(++productId);
         products.add(product);
+    }
+
+    public static void addUser(User user) {
+        user.setId(++userId);
+        users.add(user);
+    }
+
+    public static void addOrder(Order order) {
+        order.setId(++orderId);
+        orders.add(order);
+    }
+
+    public static void addCart(ShoppingCart cart) {
+        cart.setId(++cartId);
+        shoppingCarts.add(cart);
     }
 }
