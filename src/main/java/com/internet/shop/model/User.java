e02b60c7ad342949ba6790abe26036c41e066ae0
Artem Vremere <vremere_a@ukr.net>
2020-09-05 20:48:48 +0300

    add lombok

--- src/main/java/com/internet/shop/model/User.java
+++ src/main/java/com/internet/shop/model/User.java
@@ -1,5 +1,12 @@
 package com.internet.shop.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class User {
     private Long id;
     private String name;
@@ -10,47 +17,5 @@ public class User {
         this.name = name;
         this.login = login;
         this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", login='" + login + '\''
-                + ", password='" + password + '\''
-                + '}';
     }
 }
