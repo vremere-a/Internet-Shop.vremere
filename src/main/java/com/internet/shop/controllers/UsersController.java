a0f516ea905b29f5a3c7b07f56687e68bdf70747
Artem Vremere <vremere_a@ukr.net>
2020-09-09 13:21:04 +0300

    create UsersController

--- src/main/java/com/internet/shop/controllers/UsersController.java
+++ src/main/java/com/internet/shop/controllers/UsersController.java
@@ -1,15 +1,30 @@
 package com.internet.shop.controllers;
 
+import com.internet.shop.library.Injector;
+import com.internet.shop.model.User;
+import com.internet.shop.service.interfaces.UserService;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class UsersController extends HttpServlet {
+    private static Injector injector = Injector.getInstance("com.internet.shop");
+    private UserService userService = (UserService) injector.getInstance(UserService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+
+        User tom = new User("Tom", "tom111", "111");
+        User tim = new User("Tim", "tim222", "222");
+        userService.create(tom);
+        userService.create(tim);
+
+        List<User> allUsers = userService.getAll();
+        req.setAttribute("users", allUsers);
         req.getRequestDispatcher("/WEB-INF/views/users/all.jsp")
                 .forward(req, resp);
     }
--- src/main/webapp/WEB-INF/views/users/all.jsp
+++ src/main/webapp/WEB-INF/views/users/all.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -5,5 +6,22 @@
 </head>
 <body>
 <h1>All users page</h1>
+
+<table border="1">
+    <tr>
+        <td>ID</td>
+        <td>Name</td>
+    </tr>
+    <c:forEach var="user" items="${users}">
+    <tr>
+        <td>
+            <c:out value="${user.id}"/>
+        </td>
+        <td>
+            <c:out value="${user.name}"/>
+        </td>
+    </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
