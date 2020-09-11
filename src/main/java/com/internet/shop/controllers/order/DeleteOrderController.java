d65bfbdf0591c73a8fb7454d083619a64877b5bb
Artem Vremere <vremere_a@ukr.net>
2020-09-11 09:04:07 +0300

    rebase

---
+++ src/main/java/com/internet/shop/controllers/order/DeleteOrderController.java
@@ -0,0 +1,26 @@
+package com.internet.shop.controllers.order;
+
+import com.internet.shop.library.Injector;
+import com.internet.shop.service.interfaces.OrderService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/order/delete")
+public class DeleteOrderController extends HttpServlet {
+    private static Injector injector = Injector.getInstance("com.internet.shop");
+    private OrderService orderService =
+            (OrderService) injector.getInstance(OrderService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String userId = req.getParameter("id");
+        Long id = Long.valueOf(userId);
+        orderService.deleteById(id);
+        resp.sendRedirect(req.getContextPath() + "/orders/all");
+    }
+}
