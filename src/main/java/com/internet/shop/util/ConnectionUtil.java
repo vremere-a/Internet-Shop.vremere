e240da547166300494a6fafc28f8a2f3f9b75d24
Artem Vremere <vremere_a@ukr.net>
2020-09-20 23:36:14 +0300

    add ConnectionUtil and pom file update

---
+++ src/main/java/com/internet/shop/util/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package com.internet.shop.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find MySQL Driver!", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        dbProperties.put("user", "root");
+        dbProperties.put("password", "Vremere_a");
+        String url = "jdbc:mysql://localhost:3306/internet_shop?serverTimezone=UTC";
+        try {
+            Connection connection = DriverManager.getConnection(url, dbProperties);
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't establish connection to DB!", e);
+        }
+    }
+}
