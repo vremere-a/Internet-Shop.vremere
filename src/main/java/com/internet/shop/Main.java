034e438439efbc9546bf91527bf305ce23d93eaf
Artem Vremere <vremere_a@ukr.net>
2020-09-21 08:50:54 +0300

    add Main update ProductDaoJdbcImpl

---
+++ src/main/java/com/internet/shop/Main.java
@@ -0,0 +1,7 @@
+package com.internet.shop;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/internet/shop/dao/jdbc/ProductDaoJdbcImpl.java
+++ src/main/java/com/internet/shop/dao/jdbc/ProductDaoJdbcImpl.java
@@ -1,6 +1,7 @@
 package com.internet.shop.dao.jdbc;
 
 import com.internet.shop.dao.interfaces.ProductDao;
+import com.internet.shop.exeptions.DataProcessingException;
 import com.internet.shop.model.Product;
 import com.internet.shop.util.ConnectionUtil;
 
@@ -9,29 +10,52 @@ import java.util.List;
 import java.util.Optional;
 
 public class ProductDaoJdbcImpl implements ProductDao {
-    String query =
-            "INSERT INTO products (productName, price) VALUES (?, ?)";
+    private static final String QUERY_CREATE =
+            "INSERT INTO products (product_name, price) VALUES (?, ?)";
+    private static final String QUERY_GET_ID =
+            "SELECT * FROM products WHERE product_id = ?";
+    private static final String QUERY_UPDATE =
+            "UPDATE products SET productName = ?, price = ? WHERE product_id = ?";
+    private static final String QUERY_DELETE =
+            "UPDATE products SET deleted = true WHERE product_id = ?";
+    private static final String QUERY_GET_ALL =
+            "SELECT * FROM products";
+
     @Override
     public Product create(Product element) {
 
         try (Connection connection = ConnectionUtil.getConnection()) {
             PreparedStatement statement = connection
-                    .prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-
+                    .prepareStatement(QUERY_CREATE, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, element.getName());
+            statement.setDouble(2, element.getPrice());
+            statement.executeQuery();
             ResultSet resultSet = statement.getGeneratedKeys();
             if (resultSet.next()) {
                 element.setId(resultSet.getLong(1));
             }
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+            return element;
+        } catch (SQLException ex) {
+            throw new DataProcessingException("Created product " + element + " is failed!", ex);
         }
-        return element;
     }
 
     @Override
     public Optional<Product> getById(Long id) {
-        return Optional.empty();
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(QUERY_GET_ID);
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String name = resultSet.getString("product_name");
+                double price = resultSet.getDouble("price");
+                Product product = new Product(id, name, price);
+                return Optional.of(product);
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new DataProcessingException("Delete of product with id = " + id + " is failed!", e);
+        }
     }
 
     @Override
@@ -41,7 +65,14 @@ public class ProductDaoJdbcImpl implements ProductDao {
 
     @Override
     public boolean deleteById(Long id) {
-        return false;
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(QUERY_DELETE);
+            statement.setLong(1, id);
+            statement.executeQuery();
+            return true;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Delete of product with id = " + id + " is failed!", e);
+        }
     }
 
     @Override
